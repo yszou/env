f82615f948a22a8ee23925cd1911e60bee03baae
YS.Zou <yeshengzou@gmail.com>
2016-05-15 14:31:04 +0800

    添加startup

---
+++ startup/apt-get.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    python-dev \
+    libssl-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libopenjpeg-dev \
+    libpng12-dev \
+    libgif-dev \
+    libfreetype6-dev \
+    libtiff5-dev \
+    libwebp-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    libsqlite3-dev \
+    libmysqlclient-dev \
+    libpq-dev \
+    libyaml-dev \
+    libffi-dev \
+    libreadline6-dev \
+    libpcre3-dev \
+    libncursesw5-dev \
+    ctags \
+    zsh \
+    vim-gtk \
+    git \
+    python-pip \
+    swig \
+    imagemagick \
+    curl \
+    axel \
+    build-essential \
+    dkms \
+    fonts-wqy-zenhei \
+    fonts-wqy-microhei \
+    ttf-wqy-microhei \
+    ttf-wqy-zenhei \
+    xfonts-wqy \
+    gimp \
+    audacity \
+    gtk-recordmydesktop \
+    mplayer \
+    kmplayer \
+    kdenlive \
+    fcitx-rime \
+    librime-data-wubi \
+    autoconf
+
+
+
+
+
+
---
+++ startup/pip.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo pip install -vv supervisor
+sudo pip install -vv Pillow
+sudo pip install -vv lxml
+sudo pip install -vv MySQL-python
+sudo pip install -vv psycopg2
+
+sudo pip install -vv backports.ssl_match_hostname
+sudo pip install -vv tornado
+
+sudo pip install -vv SQLAlchemy
+sudo pip install -vv Pygments
+sudo pip install -vv Django
+
+sudo pip install -vv cffi
+sudo pip install -vv cryptography
+sudo pip install -vv pycparser
+sudo pip install -vv pycrypto
+sudo pip install -vv paramiko
+sudo pip install -vv Jinja2
+sudo pip install -vv ansible
+
+sudo pip install -vv zope.interface
+sudo pip install -vv Twisted
+sudo pip install -vv Scrapy
+
+sudo pip install -vv pylibmc
+sudo pip install -vv pika
+sudo pip install -vv xlrd
+sudo pip install -vv M2Crypto
+
