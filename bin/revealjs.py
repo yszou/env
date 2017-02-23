3b2dbdb223a32909ff68ae93e2e537179cae97f7
YS.Zou <yeshengzou@gmail.com>
2017-02-23 16:02:12 +0800

    revealjs工具更新

--- bin/revealjs.py
+++ bin/revealjs.py
@@ -41,6 +41,41 @@ TEMPLATE = u'''
           .reveal code { color: #52E852; }
           .reveal strong, .reveal b { color: #FF1E1E; font-weight: normal; }
           .reveal em { color: #F9843E; font-style: normal; }
+          .reveal section img { display: block; margin: 15px auto; }
+          .reveal section embed { display: block; margin: 15px auto; background: rgba(255, 255, 255, 0.12); border: 4px solid #eee; box-shadow: 0 0 10px rgba(0, 0, 0, 0.15); }
+          .reveal pre { font-size: 0.5em; width: auto; }
+          .reveal pre code { color: white; white-space: pre-wrap; line-height: 1.3em; }
+
+          .reveal pre, .reveal pre code { color: #cccccc; background-color: #303030; }
+          .reveal pre code > span.kw { color: #f0dfaf; } /* Keyword */
+          .reveal pre code > span.dt { color: #dfdfbf; } /* DataType */
+          .reveal pre code > span.dv { color: #dcdccc; } /* DecVal */
+          .reveal pre code > span.bn { color: #dca3a3; } /* BaseN */
+          .reveal pre code > span.fl { color: #c0bed1; } /* Float */
+          .reveal pre code > span.ch { color: #dca3a3; } /* Char */
+          .reveal pre code > span.st { color: #cc9393; } /* String */
+          .reveal pre code > span.co { color: #7f9f7f; } /* Comment */
+          .reveal pre code > span.ot { color: #efef8f; } /* Other */
+          .reveal pre code > span.al { color: #ffcfaf; } /* Alert */
+          .reveal pre code > span.fu { color: #efef8f; } /* Function */
+          .reveal pre code > span.er { color: #c3bf9f; } /* Error */
+          .reveal pre code > span.wa { color: #7f9f7f; font-weight: bold; } /* Warning */
+          .reveal pre code > span.cn { color: #dca3a3; font-weight: bold; } /* Constant */
+          .reveal pre code > span.sc { color: #dca3a3; } /* SpecialChar */
+          .reveal pre code > span.vs { color: #cc9393; } /* VerbatimString */
+          .reveal pre code > span.ss { color: #cc9393; } /* SpecialString */
+          .reveal pre code > span.im { } /* Import */
+          .reveal pre code > span.va { } /* Variable */
+          .reveal pre code > span.cf { color: #f0dfaf; } /* ControlFlow */
+          .reveal pre code > span.op { color: #f0efd0; } /* Operator */
+          .reveal pre code > span.bu { } /* BuiltIn */
+          .reveal pre code > span.ex { } /* Extension */
+          .reveal pre code > span.pp { color: #ffcfaf; font-weight: bold; } /* Preprocessor */
+          .reveal pre code > span.at { } /* Attribute */
+          .reveal pre code > span.do { color: #7f9f7f; } /* Documentation */
+          .reveal pre code > span.an { color: #7f9f7f; font-weight: bold; } /* Annotation */
+          .reveal pre code > span.cv { color: #7f9f7f; font-weight: bold; } /* CommentVar */
+          .reveal pre code > span.in { color: #7f9f7f; font-weight: bold; } /* Information */
 
         </style>
 	</head>
@@ -70,7 +105,7 @@ TEMPLATE = u'''
 				// More info https://github.com/hakimel/reveal.js#dependencies
 				dependencies: [
 					{ src: 'http://s.zys.me/js/revealjs/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-					{ src: 'http://s.zys.me/js/revealjs/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+					//{ src: 'http://s.zys.me/js/revealjs/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
 					{ src: 'http://s.zys.me/js/revealjs/plugin/zoom-js/zoom.js', async: true },
 					{ src: 'http://s.zys.me/js/revealjs/plugin/notes/notes.js', async: true },
                     { src: 'http://s.zys.me/js/revealjs/plugin/math/math.js', async: true }
