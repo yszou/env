caeed980f174d3db09d03d31857ebfbfea0dbb42
YS.Zou <yeshengzou@gmail.com>
2018-07-18 23:29:51 +0800

    添加apt-get-server

---
+++ startup/apt-get-server.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    python-dev \
+    libssl-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libpng-dev \
+    libgif-dev \
+    libfreetype6-dev \
+    libtiff5-dev \
+    libwebp-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    libsqlite3-dev \
+    libmysqlclient-dev \
+    libpq-dev \
+    libyaml-dev \
+    libffi-dev \
+    libreadline6-dev \
+    libpcre3-dev \
+    libncursesw5-dev \
+    ctags \
+    zsh \
+    git \
+    python-pip \
+    swig \
+    imagemagick \
+    curl \
+    axel \
+    build-essential \
+    dkms \
+    fonts-wqy-zenhei \
+    fonts-wqy-microhei \
+    ttf-wqy-microhei \
+    ttf-wqy-zenhei \
+    xfonts-wqy \
+    autoconf
+
+
+
+
+
+
